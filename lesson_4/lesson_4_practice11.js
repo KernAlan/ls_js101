07aa246192a9006a9d6d14850be20a6dc36d9841
Alan Kern <kernalan@gmail.com>
2020-11-02 02:56:25 +0000

    Adding last practice file for lesson 4

--- lesson_4/lesson_4_practice11.js
+++ lesson_4/lesson_4_practice11.js
@@ -6,54 +6,19 @@ Algorithm:
   -Create an object with the alphabet as keys and 0's as values
   -Loop through the object and test if each index has one of the string characters
   -if it does, increment value by 1
-  -delete all 0 value properties
+  -Loop through object and delete all 0 value properties
 */
 
-let statement = "The Flintstones Rock".toLowerCase();
+let statement = "The Flintstones Rock";
 
-let fixedStatement = statement.split(" ");
-let fixedStatementTwo = fixedStatement.join("");
+let result = {};
 
-let statementArr = fixedStatementTwo.split("");
+for (let counter = 0; counter < statement.length; counter += 1) {
+  let char = statement[counter];
+  if (char === ' ') continue;
 
-let alphabet = {
-  a : 0,
-  b : 0,
-  c : 0,
-  d : 0,
-  e : 0,
-  f : 0,
-  g : 0,
-  h : 0,
-  i : 0,
-  j : 0,
-  k : 0,
-  l : 0,
-  m : 0,
-  n : 0,
-  o : 0,
-  p : 0,
-  q : 0,
-  r : 0,
-  s : 0,
-  t : 0,
-  u : 0,
-  v : 0,
-  w : 0,
-  x : 0,
-  y : 0,
-  z : 0,
+  result[char] = result[char] || 0;
+  result[char] += 1;
 }
 
-for (let i = 0; i < statementArr.length; i++) {
-  let currentLetter = statementArr[i];
-    alphabet[currentLetter] += 1;
-}
-
-for (let element in alphabet) {
-  if (alphabet[element] === 0) {
-    delete alphabet[element]
-  }
-}
-
-console.log(alphabet);
+console.log(result);
